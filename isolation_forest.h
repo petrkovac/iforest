e00c99a986654af3e42229e4845b6b3c9e2de093
Kovac Peter <kovac@avast.com>
2016-07-28 10:05:28 +0200

    Use array instead of vector. Simplified and unified API. Faster anomaly score calculation. Fixed a few bugs.

--- isolation_forest.h
+++ isolation_forest.h
@@ -1,30 +1,31 @@
 /*
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 
-Copyright (c) 2016 Peter Kovac
+Copyright (c) 2016 Peter Kovac (petr.kovac09 at gmail.com)
 
-Permission is hereby  granted, free of charge, to any  person obtaining a copy
-of this software and associated  documentation files (the "Software"), to deal
-in the Software  without restriction, including without  limitation the rights
-to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
-copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
-IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
-FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
-AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
-LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
 #pragma once
 #include <stdint.h>
 #include <memory>
+#include <array>
 #include <vector>
 #include <algorithm>
 #include <random>
@@ -33,9 +34,13 @@ SOFTWARE.
 
 namespace iforest
 {
-	// Templated class for generating an uniform random number in range <min, max> inclusive.
+	// Templated class for generating an uniform random number in range <min, max>.
 	// The class is then specialized for several types, since there are some implementation
 	// specifics when using integral or floating point data types.
+	// Note: for integral types, uniform_int_distribution is used and the generated numbers
+	// are in range <min, max> (inclusive), but for floating point numbers, the
+	// uniform_real_distribution is used which generated numbers in range <min, max).
+	// In practice, this should not matter, but it is worth mentioning.
 	template <typename ValueType>
 	class UniformRandomNumber
 	{
@@ -113,13 +118,13 @@ namespace iforest
 	};
 
 	// Calculates the harmonic number for i (using simple aproximation)
-	static double CalculateH(uint32_t i)
+	static inline double CalculateH(uint32_t i)
 	{
 		return log(i) + 0.5772156649;
 	}
 
 	// Calculates the C(n) parameter needed for anomaly score calculation
-	static double CalculateC(uint32_t n)
+	static inline double CalculateC(uint32_t n)
 	{
 		if (n > 2)
 		{
@@ -136,36 +141,31 @@ namespace iforest
 		}
 	}
 
+	// Calculates the anomaly score, result is in range <0, 1>
+	static inline double CalculateAnomalyScore(uint32_t sampleCount, double avgPathLen)
+	{
+		return pow(2, -avgPathLen / CalculateC(sampleCount));
+	}
+
 	// Forward declare IsolationForest so we can
 	// make IsolationForest a friend of IsolationTree
-	template <typename ValueType> class IsolationForest;
+	template <typename ValueType, size_t DimensionCount> class IsolationForest;
 
-	template <typename ValueType>
+	template <typename ValueType, size_t DimensionCount>
 	class IsolationTree
 	{
 		static_assert(std::is_default_constructible<ValueType>::value, "ValueType must be default-constructible");
 		static_assert(std::is_integral<ValueType>::value || std::is_floating_point<ValueType>::value, "ValueType must be of integral or floating point type");
 
-		friend class IsolationForest<ValueType>;
+		static_assert(DimensionCount > 0, "DimensionCount must be greater than zero");
 
-		class Item
-		{
-			const std::vector<ValueType>* m_data;
+		friend class IsolationForest<ValueType, DimensionCount>;
 
-		public:
+	public:
 
-			Item(const std::vector<ValueType>* data) : m_data(data) {}
+		using item_t = std::array<ValueType, DimensionCount>;
 
-			const ValueType& At(size_t pos) const
-			{
-				return (*m_data)[pos];
-			}
-
-			uint32_t Dimensions() const
-			{
-				return static_cast<uint32_t>(m_data->size());
-			}
-		};
+	private:
 
 		class Node
 		{
@@ -186,9 +186,9 @@ namespace iforest
 				m_right = nullptr;
 			}
 
-			bool Build(std::mt19937& rng, std::vector<Item>& input, uint32_t first, uint32_t last, uint32_t depth, uint32_t maxDepth)
+			bool Build(std::mt19937& rng, std::vector<item_t*>& data, uint32_t first, uint32_t last, uint32_t depth, uint32_t maxDepth)
 			{
-				if (last < first || last >= input.size())
+				if (last < first || last >= data.size())
 				{
 					return false;
 				}
@@ -199,14 +199,14 @@ namespace iforest
 					return true;
 				}
 
-				std::uniform_int_distribution<uint32_t> dimension_dis(0, input[0].Dimensions() - 1);
+				std::uniform_int_distribution<uint32_t> dimension_dis(0, DimensionCount - 1);
 
 				uint32_t dim = m_dimId = dimension_dis(rng);
 
-				std::sort(input.begin() + first, input.begin() + last + 1, [&dim](const Item& left, const Item& right) { return left.At(dim) < right.At(dim); });
+				std::sort(data.begin() + first, data.begin() + last + 1, [&dim](const item_t* left, const item_t* right) { return (*left)[dim] < (*right)[dim]; });
 
-				ValueType minVal = input[first].At(dim);
-				ValueType maxVal = input[last].At(dim);
+				ValueType minVal = (*data[first])[dim];
+				ValueType maxVal = (*data[last])[dim];
 
 				if (minVal == maxVal)
 				{
@@ -214,12 +214,12 @@ namespace iforest
 					return true;
 				}
 
-				m_dimSplitValue = UniformRandomNumber<ValueType>::GenerateNext(rng, minVal + 1, maxVal);
+				m_dimSplitValue = UniformRandomNumber<ValueType>::GenerateNext(rng, minVal, maxVal);
 				uint32_t middle = first;
 
 				for (middle = first; middle <= last; middle++)
 				{
-					if (input[middle].At(dim) >= m_dimSplitValue)
+					if ((*data[middle])[dim] >= m_dimSplitValue)
 					{
 						break;
 					}
@@ -234,12 +234,12 @@ namespace iforest
 				m_left = std::unique_ptr<Node>(new Node());
 				m_right = std::unique_ptr<Node>(new Node());
 
-				if (!m_left->Build(rng, input, first, middle - 1, depth + 1, maxDepth))
+				if (!m_left->Build(rng, data, first, middle - 1, depth + 1, maxDepth))
 				{
 					return false;
 				}
 
-				if (!m_right->Build(rng, input, middle, last, depth + 1, maxDepth))
+				if (!m_right->Build(rng, data, middle, last, depth + 1, maxDepth))
 				{
 					return false;
 				}
@@ -293,7 +293,7 @@ namespace iforest
 				return m_left == nullptr || m_right == nullptr;
 			}
 
-			double GetPathLen(const std::vector<ValueType>& data, uint32_t currentDepth) const
+			double GetPathLen(const item_t& data, uint32_t currentDepth) const
 			{
 				if (IsLeaf())
 				{
@@ -311,37 +311,46 @@ namespace iforest
 			}
 		};
 
+		uint32_t m_sampleSize;
 		std::unique_ptr<Node> m_root;
 
 	public:
 
 		void Clear()
 		{
+			m_sampleSize = 0;
 			m_root.reset();
 		}
 
-		bool Build(uint32_t seed, std::vector<std::vector<ValueType>>& input, uint32_t sampleSize)
+		bool Build(uint32_t seed, std::vector<item_t>& data, uint32_t sampleSize)
 		{
 			Clear();
+
+			if (!data.size() || !sampleSize || sampleSize > static_cast<uint32_t>(data.size()))
+			{
+				return false;
+			}
+
+			m_sampleSize = sampleSize;
 
 			std::mt19937 gen(seed);
 
 			std::vector<uint32_t> sampleIds;
-			sampleIds.reserve(input.size());
+			sampleIds.reserve(data.size());
 
-			for (uint32_t i = 0; i < static_cast<uint32_t>(input.size()); i++)
+			for (uint32_t i = 0; i < static_cast<uint32_t>(data.size()); i++)
 			{
 				sampleIds.push_back(i);
 			}
 
 			std::shuffle(sampleIds.begin(), sampleIds.end(), gen);
 
-			std::vector<Item> localData;
+			std::vector<item_t*> localData;
 			localData.reserve(sampleSize);
 
 			for (uint32_t i = 0; i < sampleSize; i++)
 			{
-				localData.push_back(Item(&input[sampleIds[i]]));
+				localData.push_back(&data[sampleIds[i]]);
 			}
 
 			if (localData.empty())
@@ -363,6 +372,10 @@ namespace iforest
 		{
 			if (m_root)
 			{
+				size_t dimCnt = DimensionCount;
+				os.write(reinterpret_cast<const char*>(&dimCnt), sizeof(size_t));
+				os.write(reinterpret_cast<const char*>(&m_sampleSize), sizeof(uint32_t));
+
 				return m_root->Serialize(os);
 			}
 
@@ -373,12 +386,20 @@ namespace iforest
 		{
 			Clear();
 
-			m_root = std::move(std::unique_ptr<Node>(new Node()));
-			
+			size_t dimCnt = 0;
+			is.read(reinterpret_cast<char*>(&dimCnt), sizeof(size_t));
+			is.read(reinterpret_cast<char*>(&m_sampleSize), sizeof(uint32_t));
+
+			if (dimCnt != DimensionCount)
+			{
+				return false;
+			}
+
+			m_root = std::move(std::unique_ptr<Node>(new Node()));						
 			return m_root->Deserialize(is);
 		}
 				
-		double GetPathLen(const std::vector<ValueType>& data) const
+		double GetPathLen(const item_t& data) const
 		{
 			if (!m_root)
 			{
@@ -387,37 +408,77 @@ namespace iforest
 
 			return m_root->GetPathLen(data, 0);
 		}
+
+		double GetAnomalyScore(const item_t& data) const
+		{
+			if (!m_root)
+			{
+				return -1.0;
+			}
+
+			return pow(2, -GetPathLen(data) / CalculateC(m_sampleSize));
+		}
+
+		bool GetAnomalyScores(const std::vector<item_t>& data, std::vector<double>& scores) const
+		{
+			scores.clear();
+
+			if (!data.size())
+			{
+				return false;
+			}
+
+			if (m_trees.empty())
+			{
+				return false;
+			}
+
+			scores.resize(data.size());
+
+			for (auto i = 0; i < data.size(); i++)
+			{
+				scores[i] = pow(2, -GetPathLen(data[i]) / m_precalculatedC);
+			}
+
+			return true;
+		}
+
+		inline size_t GetDimensions() const
+		{
+			return DimensionCount;
+		}
 	};
 
-	template <typename ValueType>
+	template <typename ValueType, size_t DimensionCount>
 	class IsolationForest
 	{
 		uint32_t m_sampleSize;
-		std::vector<IsolationTree<ValueType>> m_trees;
+		double m_precalculatedC;
 
-		double CalculateAnomalyScore(uint32_t count, double avgDepth) const
-		{
-			return pow(2, -avgDepth / CalculateC(count));
-		}
-
+		std::vector<IsolationTree<ValueType, DimensionCount>> m_trees;
+				
 	public:
+
+		using item_t = std::array<ValueType, DimensionCount>;
 
 		void Clear()
 		{
 			m_sampleSize = 0;
+			m_precalculatedC = 0.0;
 			m_trees.clear();
 		}
 
-		bool Build(uint32_t treeCount, uint32_t seed, std::vector<std::vector<ValueType>>& input, uint32_t sampleSize)
+		bool Build(uint32_t treeCount, uint32_t seed, std::vector<item_t>& data, uint32_t sampleSize)
 		{
 			Clear();
 
-			if (sampleSize > static_cast<uint32_t>(input.size()) || !sampleSize)
+			if (!data.size() || !sampleSize || sampleSize > static_cast<uint32_t>(data.size()))
 			{
 				return false;
 			}
 
 			m_sampleSize = sampleSize;
+			m_precalculatedC = CalculateC(m_sampleSize);
 			
 			std::mt19937 gen(seed);
 			std::uniform_int_distribution<uint32_t> uniform_dis(0, std::numeric_limits<uint32_t>::max());
@@ -426,7 +487,7 @@ namespace iforest
 
 			for (uint32_t i = 0; i < treeCount; i++)
 			{
-				if (!m_trees[i].Build(uniform_dis(gen), input, sampleSize))
+				if (!m_trees[i].Build(uniform_dis(gen), data, sampleSize))
 				{
 					return false;
 				}
@@ -435,9 +496,14 @@ namespace iforest
 			return true;
 		}
 
-		double GetAnomalyScore(const std::vector<ValueType>& data) const
+		double GetAnomalyScore(const item_t& data) const
 		{
 			double totalPathLen = 0;
+
+			if (m_trees.empty())
+			{
+				return -1.0;
+			}
 
 			for (const auto& t : m_trees)
 			{
@@ -446,23 +512,37 @@ namespace iforest
 
 			double avgPathLen = totalPathLen / double(m_trees.size());
 
-			return CalculateAnomalyScore(m_sampleSize, avgPathLen);
+			return pow(2, -avgPathLen / m_precalculatedC);
 		}
 
-		bool GetAnomalyScores(const std::vector<std::vector<ValueType>>& input, std::vector<double>& scores) const
+		bool GetAnomalyScores(const std::vector<item_t>& data, std::vector<double>& scores) const
 		{
 			scores.clear();
+
+			if (!data.size())
+			{
+				return false;
+			}
 
 			if (m_trees.empty())
 			{
 				return false;
 			}
 
-			scores.resize(input.size());
+			scores.resize(data.size());
 
-			for (auto i = 0; i < input.size(); i++)
+			for (auto i = 0; i < data.size(); i++)
 			{
-				scores[i] = GetAnomalyScore(input[i]);
+				double totalPathLen = 0;
+
+				for (const auto& t : m_trees)
+				{
+					totalPathLen += t.GetPathLen(data[i]);
+				}
+
+				double avgPathLen = totalPathLen / double(m_trees.size());
+
+				scores[i] = pow(2, -avgPathLen / m_precalculatedC);
 			}
 
 			return true;
@@ -477,6 +557,8 @@ namespace iforest
 
 			uint32_t treeCount = static_cast<uint32_t>(m_trees.size());
 			
+			size_t dimCnt = DimensionCount;
+			os.write(reinterpret_cast<const char*>(&dimCnt), sizeof(size_t));
 			os.write(reinterpret_cast<const char*>(&treeCount), sizeof(uint32_t));
 			os.write(reinterpret_cast<const char*>(&m_sampleSize), sizeof(uint32_t));
 			
@@ -501,9 +583,18 @@ namespace iforest
 			Clear();
 
 			uint32_t treeCount = 0;
-
+			size_t dimCnt = 0;
+			
+			is.read(reinterpret_cast<char*>(&dimCnt), sizeof(size_t));
 			is.read(reinterpret_cast<char*>(&treeCount), sizeof(uint32_t));
 			is.read(reinterpret_cast<char*>(&m_sampleSize), sizeof(uint32_t));
+
+			if (dimCnt != DimensionCount)
+			{
+				return false;
+			}
+
+			m_precalculatedC = CalculateC(m_sampleSize);
 
 			if (!is.good())
 			{
@@ -521,6 +612,11 @@ namespace iforest
 			}
 
 			return true;
+		}
+
+		size_t GetDimensions() const
+		{
+			return DimensionCount;
 		}
 	};
 };
